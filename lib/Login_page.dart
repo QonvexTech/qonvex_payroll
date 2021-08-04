358dc1113adf0b5a936f56d88d34d340798e2aa4
Licile Salomon <ilicile@gmail.com>
2021-08-04 11:26:53 +0800

    api

--- lib/Login_page.dart
+++ lib/Login_page.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:qonvex_payroll/landing_page.dart';
+import 'package:qonvex_payroll/services/auth_service.dart';
 
 import 'register.dart';
 
@@ -13,7 +17,10 @@ class _LoginPageState extends State<LoginPage> {
   bool _passwordVisible = false;
   final TextEditingController _UserPasswordController =
       new TextEditingController();
-
+  final TextEditingController _emailController =
+      new TextEditingController();
+  final AuthService _authService = AuthService();
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     MediaQueryData deviceInfo = MediaQuery.of(context);
@@ -24,123 +31,144 @@ class _LoginPageState extends State<LoginPage> {
     print(size.width);
     return GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-            body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          height: size.height,
-          width: size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(
-              image: AssetImage("assets/background.png"),
-              alignment: Alignment.topCenter,
-            ),
-          ),
-          child: ListView(children: [
-            SizedBox(
-              height: 130,
-            ),
-            Text('Welcome\nBack',
-                style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    fontSize: 40,
-                    color: Colors.white)),
-            SizedBox(
-              height: 140,
-            ),
-            Container(
-              width: double.infinity,
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Email Address'),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              child: TextField(
-                controller: _UserPasswordController,
-                obscureText: !_passwordVisible,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _passwordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      size: 17,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
-                  ),
+        child: Stack(
+          children: [
+            Scaffold(
+                body: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              height: size.height,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                  image: AssetImage("assets/background.png"),
+                  alignment: Alignment.topCenter,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Sign in',
+              child: ListView(children: [
+                SizedBox(
+                  height: 130,
+                ),
+                Text('Welcome\nBack',
                     style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.w900,
+                        fontSize: 40,
+                        color: Colors.white)),
+                SizedBox(
+                  height: 140,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email Address'),
                   ),
-                  IconButton(
-                    icon: CircleAvatar(
-                      child: Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        color: Colors.white,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: TextField(
+                    controller: _UserPasswordController,
+                    obscureText: !_passwordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          size: 17,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
                       ),
-                      backgroundColor: Colors.grey,
                     ),
-                    iconSize: 50,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
                   ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: AlignmentDirectional.centerStart,
-              child: TextButton(
-                onPressed: () {},
-                child: Text('Forgot Password?'),
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.zero,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sign in',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                        icon: CircleAvatar(
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Colors.grey,
+                        ),
+                        iconSize: 50,
+                        onPressed: () {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          _authService.login(email: _emailController.text, password: _UserPasswordController.text).then((value) {
+                            if(value){
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> Landing_Page()));
+                            }
+                          }).whenComplete(() => setState(() => _isLoading = false));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Forgot Password?'),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Don't have an account?",
+                      ),
+                      FlatButton(
+                          textColor: Colors.blue,
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                          onPressed: () =>
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => Register(),
+                              )))
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ),
+              ]),
+            )),
+            _isLoading ? Container(
+              color: Colors.black54,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 4,sigmaY: 4),
+                child: Center(
+                  child: CircularProgressIndicator(),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "Don't have an account?",
-                  ),
-                  FlatButton(
-                      textColor: Colors.blue,
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(fontSize: 17),
-                      ),
-                      onPressed: () =>
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => Register(),
-                          )))
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            ),
-          ]),
-        )));
+            ) : Container()
+          ],
+        ));
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -6,10 +6,18 @@ import 'package:qonvex_payroll/globals/logged_user.dart';
 import 'package:qonvex_payroll/models/user_model.dart';
 
 class AuthService{
-  Future<bool> login() async{
+  Future<bool> login({required String email, required String password}) async{
     try{
-      return await http.post(Uri.parse("${API.route}/login")).then((response) {
+      return await http.post(Uri.parse("${API.domain}api/user/login"),headers: {
+        "accept" : "application/json"
+      },
+      body: {
+        "email" : email,
+        "password" : password,
+      }
+      ).then((response) {
         var data = json.decode(response.body);
+        print(data.toString());
         if(response.statusCode == 200){
           LoggedUser.details = UserModel.fromJson(data['user']);
           LoggedUser.accessToken = "${data['access_token']}";
